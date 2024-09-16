40c1b20ff8afb8bf7e430f86c49a47fdb2a7f473
ENdermarMC <marcosdursan@gmail.com>
2024-09-16 15:32:39 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+Swal.fire ({
+     title: '<span class="fuente">Hola</span>',
+	 //text:'<span class="fuente">Espero que te guste la página web de Programmers</span>',
+	 html: '<span class="fuente">Espero que te guste la página web de Programmers</span>',
+	icon: 'success',
+	// confirmButtonText:
+	footer:'<span class="morado">¿Sabias que programers está hecho por niños de 11 años?</span> ',
+	// width:
+	// padding:
+	// background:
+	grow: 'column',
+	// backdrop:
+	   timer: 3000,
+	 timerProgressBar: true,
+	// toast: true,
+	// position:
+	 allowOutsideClick: false,
+	 allowEscapeKey: false,
+	 allowEnterKey: false,
+	 //stopKeydownPropagation: false,
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+	
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+     //closeButton: false
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+	// 	confirmButton:
+	// 	cancelButton:
+	// 	footer:	
+
+	 showConfirmButton: false,
+	// confirmButtonColor:
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+	
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
